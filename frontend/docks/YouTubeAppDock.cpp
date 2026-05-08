677a0d870428ea53af2f659f0d38dfa3b7447481
Warchamp7 <warchamp2003@hotmail.com>
2026-05-08 14:34:57 -0400

    frontend: Avoid reinterpret_cast when casting QObjects
    When casting between QObjects, qobject_cast should always be used for dynamic
    casts. This increases performance as there is no RTTI (Run Time Type
    Information) with qobject_cast, like there is with dynamic_cast. In other cases
    where we are sure or assume the cast is valid, use static_cast.
    
    Using reinterpret_cast is bad practice, as you should use it in very
    specific cases.
    
    Co-Authored-By: Clayton Groeneveld <19962531+cg2121@users.noreply.github.com>

--- frontend/docks/YouTubeAppDock.cpp
+++ frontend/docks/YouTubeAppDock.cpp
@@ -381,7 +381,7 @@ YoutubeApiWrappers *YouTubeAppDock::GetYTApi()
 {
 	Auth *auth = OBSBasic::Get()->GetAuth();
 	if (auth) {
-		YoutubeApiWrappers *apiYouTube(dynamic_cast<YoutubeApiWrappers *>(auth));
+		YoutubeApiWrappers *apiYouTube(static_cast<YoutubeApiWrappers *>(auth));
 		if (apiYouTube) {
 			return apiYouTube;
 		} else {
